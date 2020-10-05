8807f5077a522165e96b82cc56a35f42db8f3382
Ho Quang Linh <hoquanglinh2710@gmail.com>
2020-10-05 14:53:52 +0700

    first commit

---
+++ HocKteam/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HocKteam
+{
+    class Person
+    {
+        private string name;
+        private int toan;
+        private int van;
+        public string Name
+        {
+            set
+            {
+                this.name = value;
+            }
+            get
+            {
+                return this.name;
+            }
+        }
+        public int Toan
+        {
+            set { this.Toan = value; }
+            get { return this.toan; }
+        }
+        public int Van
+        {
+            set { this.van = value; }
+            get { return this.toan; }
+        }
+    }
+}
---
+++ HocKteam/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Markup;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace HocKteam
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Nhap duong dan: ");
+            string filePath = @"D:\baitap.txt";
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            for(int i=0;i<lines.Count;i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+
+            Person person = new Person();
+            person.Name = lines[0];
+            Console.WriteLine(person.Name);
+            string[] entries = lines[0].Split(",");
+            Console.WriteLine(entries[1]);
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
